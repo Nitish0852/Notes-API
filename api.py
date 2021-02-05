1952c8fa3eb1e33ea1c6d9c1dc34ae1a0036be40
Nitish <43701468+Nitish0852@users.noreply.github.com>
2021-02-05 21:55:34 +0530

    Flask-RESTful API

---
+++ api.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+NOTES = {
+    'note1': {'title': 'Commonly used HTTP methods', 'content' : ' GET, POST, PUT, DELETE' },
+    'note2': {'title': 'CRUD', 'content': 'Create | Read | Update | Delete'},
+    'note3': {'title': 'Flask-RESTful','content': 'An Extension for Flask that adds support for quickly building REST APIs.'},
+}
+
+
+def abort_if_note_doesnt_exist(note_id):
+    if note_id not in NOTES:
+        abort(404, message="Note {} doesn't exist".format(note_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('title',location='form')
+parser.add_argument('content', location='form')
+
+# DELETE
+# shows a single note item and lets you delete a note item
+class MyNote(Resource):
+    def get(self, note_id):
+        abort_if_todo_doesnt_exist(note_id)
+        return NOTES[note_id]
+
+    def delete(self, note_id):
+        abort_if_todo_doesnt_exist(note_id)
+        del NOTES[note_id]
+        return '', 204
+
+    def put(self, note_id):
+        args = parser.parse_args()
+        note = {'title': args['title'], 'content':args['content']}
+        NOTES[note_id] = note
+        return note, 201
+
+# POST
+# shows a list of all NOTES, and lets you POST to add 
+class MyNotebook(Resource):
+    def get(self):
+        return NOTES
+
+    def post(self):
+        args = parser.parse_args()
+        note_id = int(max(NOTES.keys()).lstrip('note')) + 1
+        note_id = 'note%i' % note_id
+        
+        NOTES[note_id] = {'title': args['title'], 'content':args['content']}
+        return NOTES[note_id], 201
+
+
+# API Resource routing
+api.add_resource(MyNotebook, '/mynotebook')
+api.add_resource(MyNote, '/mynotebook/<note_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
